175ac2aeb07adbc1db37cb95fcdafd0dadcf30bd
D4mi4n0 <damiano.montanaro@studenti.unicam.it>
2025-03-20 01:46:54 +0100

    Aggiunte modifiche e perfezionamento dell'UI

--- StruttureMarche/Pages/Ricerca.cshtml.cs
+++ StruttureMarche/Pages/Ricerca.cshtml.cs
@@ -1,20 +1,61 @@
+using Marche.modelli;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StruttureMarche.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace StruttureMarche.Pages
 {
     public class RicercaModel : PageModel
     {
+        private const int PageSize = 10;
+
         [BindProperty]
         public RicercaViewModel Ricerca { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Comune { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Provincia { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
+        public int TotalPages { get; set; }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            var strutture = await GetFilteredStrutture(Comune, Provincia);
+            TotalPages = (int)Math.Ceiling(strutture.Count / (double)PageSize);
+
             Ricerca = new RicercaViewModel
             {
-                Strutture = new List<Marche.modelli.ModelliServiziMarche>()
+                Strutture = strutture.Skip((PageNumber - 1) * PageSize).Take(PageSize).ToList()
             };
+        }
+
+        private async Task<List<Marche.modelli.ModelliServiziMarche>> GetFilteredStrutture(string comune, string provincia)
+        {
+            var allStrutture = await GetAllStrutture(); // Metodo per ottenere tutte le strutture
+            var filteredStrutture = allStrutture.AsQueryable();
+
+            if (!string.IsNullOrEmpty(comune))
+            {
+                filteredStrutture = filteredStrutture.Where(s =>
+                    s.Comune.Contains(comune, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (!string.IsNullOrEmpty(provincia))
+            {
+                filteredStrutture = filteredStrutture.Where(s =>
+                    s.Provincia.Contains(provincia, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return filteredStrutture.ToList();
+        }
+
+        private async Task<List<Marche.modelli.ModelliServiziMarche>> GetAllStrutture()
+        {
+            var strutture = await FunzioniInterrogazioniServiziMarche.DaiTuttiIServizi();
+            return strutture.ToList();
         }
     }
 }
--- StruttureMarche/ViewModels/RicercaViewModel.cs
+++ StruttureMarche/ViewModels/RicercaViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace StruttureMarche.ViewModels
+﻿using Marche.modelli;
+
+namespace StruttureMarche.ViewModels
 {
     public class RicercaViewModel
     {
-        public string Denominazione { get; set; }
-        public string Comune { get; set; }
-        public string Provincia { get; set; }
-        public List<Marche.modelli.ModelliServiziMarche> Strutture { get; set; }
+        public string Denominazione { get; set; } // Add this line
+        public string Comune { get; set; } // Add this line
+        public string Provincia { get; set; } // Add this line
+        public List<ModelliServiziMarche> Strutture { get; set; }
     }
+
 }
